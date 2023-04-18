d1abbb948f31de3fc475bae427c351ed7cd79be3
Ariele Piziali <francescoariele.piziali@studenti.unimi.it>
2023-04-18 09:41:49 +0200

    Nuova sistemazione aggiornamento testi

--- app/pages/home.py
+++ app/pages/home.py
@@ -23,9 +23,9 @@ layout = html.Div([
                 "L’Italia viene monitorata nella sua capacità di garantire e promuovere i diritti di queste categorie sociali attraverso una serie di **30 Indicatori**, raggruppati in **15 Dimensioni**, a loro volta divise in **3 Sottoindici**: Contesto, Bambini e Donne che aggregati forniscono un **Indice generale.**"), 
                 dcc.Markdown("**L’Indice Mai più invisibili è uno strumento che:**"),
                 dbc.ListGroup([
-                    dbc.ListGroupItem(dcc.Markdown("** * Valuta il progresso dei territori** alla luce delle condizioni di inclusione che riescono a garantire alle categorie più vulnerabili: donne, bambini e adolescenti.")),# color="#FFF8B2"),
-                    dbc.ListGroupItem(dcc.Markdown("** * Tiene conto delle condizioni di donne e minori in maniera congiunta**, interdipendente e simultanea, riconoscendo un nesso tra l’implementazione dei diritti di una categoria sul benessere dell’altra e viceversa.")), # color="#E0F2FD"),
-                    dbc.ListGroupItem(dcc.Markdown("** * Considera il concetto di inclusione al di là di un’accezione dello sviluppo puramente economicistica,** aprendo a una visione ampia, multidimensionale, dinamica e positiva, personale, sociale e intersezionale, universale."))# color='#B8DCCA') #(in linea con l’Agenda 2030 e le cosiddette 5P: People, Planet, Prosperity, Partnership, Peace)
+                    dbc.ListGroupItem(dcc.Markdown("**Valuta il progresso dei territori** alla luce delle condizioni di inclusione che riescono a garantire alle categorie più vulnerabili: donne, bambini e adolescenti.")),# color="#FFF8B2"),
+                    dbc.ListGroupItem(dcc.Markdown("**Tiene conto delle condizioni di donne e minori in maniera congiunta**, interdipendente e simultanea, riconoscendo un nesso tra l’implementazione dei diritti di una categoria sul benessere dell’altra e viceversa.")), # color="#E0F2FD"),
+                    dbc.ListGroupItem(dcc.Markdown("**Considera il concetto di inclusione al di là di un’accezione dello sviluppo puramente economicistica,** aprendo a una visione ampia, multidimensionale, dinamica e positiva, personale, sociale e intersezionale, universale."))# color='#B8DCCA') #(in linea con l’Agenda 2030 e le cosiddette 5P: People, Planet, Prosperity, Partnership, Peace)
                 ], flush=False)
             ], lg = 6, xs = 12),
             dbc.Col(dcc.Graph(figure=display_map_home(), responsive=True, style ={"height":"80vh"}), lg = 6, xs = 12, align='top')        
